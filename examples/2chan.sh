98f1198a4fa73b45bd7c5aae04639068cf74a38d
Vladislav Zavjalov <slazav@altlinux.org>
2016-05-22 09:22:32 +0300

    add examples

---
+++ examples/2chan.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -x
+
+PATH=$PATH:..
+
+stsdb -d . create 2ch
+
+stsdb -d . put 2ch 1234567890 0.1 2
+stsdb -d . put 2ch 1234567900 0.2 3
+stsdb -d . put 2ch 1234567910 0.3 4
+stsdb -d . put 2ch 1234567910 0.4
+
+stsdb -d . get_interp 2ch 1234567895
+
+stsdb -d . get_prev  2ch
+stsdb -d . get_next  2ch
+stsdb -d . get_range 2ch:0
+stsdb -d . get_range 2ch:1
+
+stsdb -d . get_prev 2ch:0
+stsdb -d . get_prev 2ch:10
+
+stsdb -d . delete 2ch
---
+++ examples/simple.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+PATH=$PATH:..
+stsdb -d . create pressure INT16 "Some text"
+stsdb -d . delete pressure
+
+stsdb -d . create pressure DOUBLE "Some text"
+stsdb -d . rename pressure press
+stsdb -d . set_descr press "Measured pressure"
+
+stsdb -d . info press
+stsdb -d . list
+
+stsdb -d . put press  1234567890 0.1
+stsdb -d . put press  1234567900 0.2
+stsdb -d . get_interp press 1234567895
+
+stsdb -d . put press now 0.1
+stsdb -d . put press now 0.2
+stsdb -d . put press now 0.3
+stsdb -d . get_prev press
+stsdb -d . get_next press
+stsdb -d . get_range press
+
+stsdb -d . delete press
