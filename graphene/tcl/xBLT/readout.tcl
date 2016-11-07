e9d3e6ad3cc7e9d463e2ce3563d8bab3919d79e3
Vladislav Zavjalov <slazav@altlinux.org>
2016-11-07 09:33:03 +0200

    xblt/readout: fix command quoting

--- graphene/tcl/xBLT/readout.tcl
+++ graphene/tcl/xBLT/readout.tcl
@@ -124,7 +124,7 @@ proc xblt::readout::format {graph axdir ename eval {ax ""}} {
     }
     set axcmd [$graph axis cget $ax -command]
     if {$axcmd ne ""} {
-	set astr [$axcmd $graph $eval]
+	set astr [list $axcmd $graph $eval]
 	if {$str ne ""} {
 	    set str "$astr ($str)"
 	} else {
