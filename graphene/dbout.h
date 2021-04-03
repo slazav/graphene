537fd13f5ac8ad15731c901bfddfbdf22073e80c
Vladislav Zavjalov <slazav@altlinux.org>
2021-04-03 16:27:44 +0100

    dbout.h: update comments

--- graphene/dbout.h
+++ graphene/dbout.h
@@ -11,17 +11,9 @@
 // Class for an extended dataset object.
 //
 // Extended dataset can be just a database name, but it can also
-// contain a column, a filter, and some secondary databases
-//
-// Data are taken from the main database, then data from secondary databases
-// are interpolated (using the GET command) and added. Then the multi-column
-// result is filtered through the filter.
-//
-// Example of the extended dataset:
-//   dba:2 + dbb:3 + dbc:1 | flt
-// This means that data should be found in the 2nd column of dba,
-// then all data from dbb and first column of dbc are interpolated
-// to the data points, then all data should go through filter program flt.
+// contain a column, or a filter:
+// <name>:<column>
+// <name>:<filter>
 //
 class DBout {
   public:
